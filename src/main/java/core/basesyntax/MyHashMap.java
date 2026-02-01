616735a1e1ee312151b2c8f5ccd625c1e60d971b
Alex <ursusandwolf@protonmail.com>
2026-02-02 00:43:13 +0200

    putVal inner method #todo

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -7,9 +7,20 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     static final int DEFAULT_INITIAL_CAPACITY = 1 << 4; // aka 16
     static final float DEFAULT_LOAD_FACTOR = 0.75f;
 
+    private final Node<K, V>[] table;
+    private int size;
+    private float loadFactor;
+    private int threshold;
+
+    public MyHashMap() {
+
+        this.size = DEFAULT_INITIAL_CAPACITY;
+        this.table = new Node[this.size];
+    }
+
     @Override
     public void put(K key, V value) {
-
+        putVal(hash(key), key, value);
     }
 
     @Override
@@ -20,6 +31,55 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public int getSize() {
         return 0;
+    }
+
+    static final int hash(Object key) {
+        int h;
+        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+    }
+
+    final V putVal(int hash, K key, V value) {
+        boolean evict = true;
+        Node<K, V>[] tab;
+        Node<K, V> p;
+        int n, i;
+        if ((tab = table) == null || (n = tab.length) == 0)
+            n = (tab = resize()).length;
+        if ((p = tab[i = (n - 1) & hash]) == null)
+            tab[i] = newNode(hash, key, value, null);
+        else {
+            Node<K, V> e;
+            K k;
+            if (p.hash == hash &&
+                    ((k = p.key) == key || (key != null && key.equals(k))))
+                e = p;
+            else if (p instanceof TreeNode)
+                e = ((TreeNode<K, V>) p).putTreeVal(this, tab, hash, key, value);
+            else {
+                for (int binCount = 0; ; ++binCount) {
+                    if ((e = p.next) == null) {
+                        p.next = newNode(hash, key, value, null);
+                        if (binCount >= TREEIFY_THRESHOLD - 1) // -1 for 1st
+                            treeifyBin(tab, hash);
+                        break;
+                    }
+                    if (e.hash == hash &&
+                            ((k = e.key) == key || (key != null && key.equals(k))))
+                        break;
+                    p = e;
+                }
+            }
+            if (e != null) { // existing mapping for key
+                V oldValue = e.value;
+                e.value = value;
+                afterNodeAccess(e);
+                return oldValue;
+            }
+        }
+        if (++size > threshold)
+            resize();
+        afterNodeInsertion(evict);
+        return null;
     }
 
     static class Node<K, V> implements Map.Entry<K, V> {
