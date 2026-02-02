9830a3b94913a7cbd29bfc57bdb549277236627d
alex <ursusandwolf@protonmail.com>
2026-02-02 11:20:18 +0200

    class Entry, constructor, index

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,10 +1,31 @@
 package core.basesyntax;
 
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+
+    private Entry<K, V>[] table;
+    private int size = 0;
+
+    @SuppressWarnings("unchecked")
+    public MyHashMap() {
+        table = new Entry[DEFAULT_CAPACITY];
+    }
 
     @Override
     public void put(K key, V value) {
+        int index = index(key);
+        Entry<K,V> entry = table[index];
+        while (entry != null) {
+            if (keyEquals(entry.key, key)) {
+                entry.value = value;
+            }
+        }
 
+    }
+
+    private boolean keyEquals(K key, K key1) {
+        return key == key1 || (key != null && key.equals(key1));
     }
 
     @Override
@@ -16,4 +37,24 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     public int getSize() {
         return 0;
     }
+
+    private int index(K key) {
+        if (key == null) {
+            return 0;
+        }
+        return Math.abs(key.hashCode()) % table.length;
+    }
+
+
+    class Entry<K, V> {
+        final K key;
+        V value;
+        Entry<K, V> next;
+
+        Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
 }
